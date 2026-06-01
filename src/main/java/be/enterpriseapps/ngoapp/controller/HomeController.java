4294733c33c0fc9efea55e09e3d3e6ef0bdd89c6
Samiahaj <hajjisamia10@gmail.com>
2026-06-01 19:40:33 +0200

    Evenementenoverzicht op startpagina toegevoegd

--- src/main/java/be/enterpriseapps/ngoapp/controller/HomeController.java
+++ src/main/java/be/enterpriseapps/ngoapp/controller/HomeController.java
@@ -1,15 +1,26 @@
 package be.enterpriseapps.ngoapp.controller;
-
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import be.enterpriseapps.ngoapp.service.EventService;
 
 @Controller
 public class HomeController {
 
+    private final EventService eventService;
+
+    public HomeController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("events", eventService.getEvents());
+
         return "index";
     }
+
     @GetMapping("/about")
     public String about() {
         return "about";
